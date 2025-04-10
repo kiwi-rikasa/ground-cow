840686a3214bae2500afe125c7cca0a316f9c3a5
Yuting <chenyuting1008@gmail.com>
2025-04-10 22:04:21 +0800

    fix(frontend): remove redundant signin page

--- frontend/components/auth-dialog.tsx
+++ frontend/components/auth-dialog.tsx
@@ -63,7 +63,7 @@ export default function AuthDialog() {
               Sign Out
             </Button>
           ) : (
-            <Button className="round-xl" onClick={() => signIn()}>
+            <Button className="round-xl" onClick={() => signIn("google")}>
               Sign In
             </Button>
           )}
