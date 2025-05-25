2d3cf1e664425ca18f01086fd90a95985f0ccd1f
GUAN MING <guanmingchiu@gmail.com>
2025-05-25 16:35:14 +0800

    chore: update GitHub Actions workflow to support multiple OS

--- backend/scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-set -x
-
-uv run ruff check app scripts --fix
-uv run ruff format app scripts
--- backend/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-uv run coverage run --source=app -m pytest
-uv run coverage report --show-missing
-uv run coverage html --title "${@-coverage}"
