c24fbf9fa97bdb800bfb9087ce610fe008fd2d85
GUAN MING <guanmingchiu@gmail.com>
2025-05-27 05:21:22 +0800

    feat(airflow): add alert closer dag

--- airflow/src/data/alert_repository.py
+++ airflow/src/data/alert_repository.py
@@ -41,3 +41,38 @@ def save_alert(alert: Alert) -> tuple[int, dict]:
     except Exception as e:
         log.error(f"Error saving alert data: {e}")
         raise
+
+
+def get_active_alerts() -> list[Alert] | None:
+    log.info("Getting active alert from backend...")
+    try:
+        response = requests.get(
+            f"{ALERT_API_URL}?alert_state=active",
+            headers=config.BACKEND_ACCESS_HEADER,
+            timeout=10,
+        )
+        response.raise_for_status()
+        result = response.json()
+        log.info(f"Got active alert from backend: {result}")
+        return result.get("data", [])
+    except Exception as e:
+        log.error(f"Error getting active alert from backend: {e}")
+        raise
+
+
+def close_alert(alert_id: int) -> Alert | None:
+    log.info("Closing alert from backend...")
+    try:
+        response = requests.patch(
+            f"{ALERT_API_URL}/{alert_id}",
+            json={"alert_state": "closed"},
+            headers=config.BACKEND_ACCESS_HEADER,
+            timeout=10,
+        )
+        response.raise_for_status()
+        result = response.json()
+        log.info(f"Closed alert from backend: {result}")
+        return result
+    except Exception as e:
+        log.error(f"Error closing alert from backend: {e}")
+        raise
